ef064b7d26505415de82519a68a76e6177ccc444
Facundo Dominguez <facundoadominguez2009@gmail.com>
2026-08-28 21:15:48 -0300

    Add files via upload

--- En desarrollo/Programación-post-regionales/ESP32_Seguidor_de_Linea/lib/Motores/Motores.h
+++ En desarrollo/Programación-post-regionales/ESP32_Seguidor_de_Linea/lib/Motores/Motores.h
@@ -5,9 +5,16 @@
 
 class Motores{
 private:
+//----Motor izquierdo----
 uint8_t IN1A;
 uint8_t IN1B;
+//----Motor derecho----
 uint8_t IN2A;
 uint8_t IN2B;
+
+int frequency = 1000;
+int resolution = 8;
+
+
 }
 #endif
